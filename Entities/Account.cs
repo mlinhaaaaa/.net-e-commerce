439881d9ec28831871fb576949e187bcc5b2a958
Hồng Phát <nphat6872@gmail.com>
2024-07-16 11:18:03 +0700

    16/07

--- Entities/Account.cs
+++ Entities/Account.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace e_commmerce.Entities;
 
@@ -8,28 +7,15 @@ public partial class Account
 {
     public int Uid { get; set; }
 
-    [Required(ErrorMessage = "Username is required")]
-    [StringLength(50, ErrorMessage = "Username cannot be longer than 50 characters")]
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
     public string User { get; set; } = null!;
 
-    [Required(ErrorMessage = "Password is required")]
-    [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 50 characters")]
-    [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,50}$", ErrorMessage = "contain at least one uppercase letter, one number, and one special character.")]
     public string Pass { get; set; } = null!;
 
-    [Compare("Pass", ErrorMessage = "Password and confirmation password do not match")]
-    public string ConfirmPassword { get; set; } = null!;
-
-    [Required(ErrorMessage = "First name is required")]
-    [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters")]
-    public string FirstName { get; set; } = null!;
-
-    [Required(ErrorMessage = "Last name is required")]
-    [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters")]
-    public string LastName { get; set; } = null!;
+    public string? ConfirmPassword { get; set; }
 
     public int IsAdmin { get; set; }
-
-    public virtual ICollection<BillingAddress> BillingAddresses { get; set; } = new List<BillingAddress>();
-
 }
--- Entities/ShopContext.cs
+++ Entities/ShopContext.cs
@@ -17,66 +17,51 @@ public partial class ShopContext : DbContext
 
     public virtual DbSet<Account> Accounts { get; set; }
 
-    public virtual DbSet<BillingAddress> BillingAddresses { get; set; }
-
     public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<New> News { get; set; }
 
     public virtual DbSet<Product> Products { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-MC950J7\\NVMINH;Database=shop;User Id=sa;Password=Minh1213@;TrustServerCertificate=True;Trusted_Connection=True;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-DITISJL\\MYSQL;user id=sa; password= hongphat@3008;Database=shop; Trusted_Connection=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Account>(entity =>
         {
-            entity.HasKey(e => e.Uid).HasName("PK__Accounts__C5B69A4AFC105B6D");
+            entity.HasKey(e => e.Uid);
 
             entity.Property(e => e.FirstName).HasMaxLength(50);
             entity.Property(e => e.LastName).HasMaxLength(50);
-            entity.Property(e => e.Pass).HasColumnName("pass");
-        });
-
-        modelBuilder.Entity<BillingAddress>(entity =>
-        {
-            entity.ToTable("BillingAddress");
-
-            entity.Property(e => e.Id)
-                .ValueGeneratedNever()
-                .HasColumnName("id");
-            entity.Property(e => e.City).HasMaxLength(250);
-            entity.Property(e => e.CompanyName).HasMaxLength(250);
-            entity.Property(e => e.Country).HasMaxLength(250);
-            entity.Property(e => e.County).HasMaxLength(250);
-            entity.Property(e => e.Email).HasMaxLength(250);
-            entity.Property(e => e.FirstName).HasMaxLength(250);
-            entity.Property(e => e.LastName).HasMaxLength(250);
-            entity.Property(e => e.Phone).HasMaxLength(250);
-            entity.Property(e => e.Streetaddress).HasMaxLength(250);
-
-            entity.HasOne(d => d.AccountU).WithMany(p => p.BillingAddresses)
-                .HasForeignKey(d => d.AccountUid)
-                .HasConstraintName("FK_BillingAddress_Accounts");
         });
 
         modelBuilder.Entity<Category>(entity =>
         {
-            entity.HasKey(e => e.Cid).HasName("PK__Categori__C1FFD861C159A1EA");
+            entity.HasKey(e => e.Cid);
 
             entity.Property(e => e.Name).HasMaxLength(100);
         });
 
+        modelBuilder.Entity<New>(entity =>
+        {
+            entity.HasKey(e => e.IdN);
+
+            entity.ToTable("New");
+
+            entity.Property(e => e.IdN).ValueGeneratedNever();
+            entity.Property(e => e.Description).HasMaxLength(200);
+        });
+
         modelBuilder.Entity<Product>(entity =>
         {
-            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC07A2793DA3");
-
-            entity.Property(e => e.CateId).HasColumnName("cateID");
+            entity.Property(e => e.CateId).HasColumnName("CateID");
             entity.Property(e => e.Price).HasColumnType("money");
 
             entity.HasOne(d => d.Cate).WithMany(p => p.Products)
                 .HasForeignKey(d => d.CateId)
-                .HasConstraintName("FK__Products__cateID__3B75D760");
+                .HasConstraintName("FK_Products_Categories");
         });
 
         OnModelCreatingPartial(modelBuilder);
