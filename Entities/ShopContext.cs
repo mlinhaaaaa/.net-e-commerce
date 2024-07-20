86980efca23d2ccb8f997716c7e2b8e2c11545a4
guenhiep <guenhiep@gmail.com>
2024-07-20 14:24:44 +0700

    Update huuhiep code singleProduct and ImagePath

--- Entities/ShopContext.cs
+++ Entities/ShopContext.cs
@@ -33,7 +33,7 @@ public partial class ShopContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-MC950J7\\NVMINH;Database=shop;User Id=sa;Password=Minh1213@;TrustServerCertificate=True;Trusted_Connection=True;");
+        => optionsBuilder.UseSqlServer("Server=GUENHIEPPC;Database=shop;User Id=sa;Password=Hiep7895123;TrustServerCertificate=True;Trusted_Connection=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
